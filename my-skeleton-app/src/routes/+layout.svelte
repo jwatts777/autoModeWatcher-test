4236ad87bf3b21baf5f70dd576d9b12b906db5b4
jwatts777 <watts.jordan777@gmail.com>
2023-11-23 23:53:36 -0600

    ...

---
+++ my-skeleton-app/src/routes/+layout.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppBar, setInitialClassState } from '@skeletonlabs/skeleton';
+	import { autoModeWatcher } from '@skeletonlabs/skeleton';
+	import { modeOsPrefers, modeUserPrefers, modeCurrent } from '@skeletonlabs/skeleton';
+	setInitialClassState();
+</script>
+
+<!-- App Shell -->
+<svelte:head>{@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}</svelte:head>
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Skeleton</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<div>{$modeOsPrefers ? "Light" : "Dark"}</div>
+				<div>{$modeUserPrefers ? "Light" : "Dark"}</div>
+				<div>{$modeCurrent}</div>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://discord.gg/EXqV7W8MtY"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Discord
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/SkeletonUI"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Twitter
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+				<div class=' btn variant-filled-primary' on:click={() => autoModeWatcher()}></div>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
